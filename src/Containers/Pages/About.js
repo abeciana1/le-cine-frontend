686088754a58667ee65c3abbce6d8fc31bec4de6
Alex <alex.beciana@gmail.com>
2021-01-21 17:51:45 -0500

    Finished new copy for About page

--- src/Containers/Pages/About.js
+++ src/Containers/Pages/About.js
@@ -4,65 +4,109 @@ import MediaQuery from 'react-responsive'
 const About = (props) => {
     
     return (
-        <React.Fragment>
+      <React.Fragment>
         <MediaQuery maxWidth={999}>
-                <div style={{ "width": "80%", "marginLeft": "auto", "marginRight": "auto", "fontSize": "1rem", "display": "inline" }}>
-                <h1>About Us</h1>
-                <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at elementum. Morbi enim nunc faucibus a pellentesque. Velit laoreet id donec ultrices tincidunt arcu non sodales. Ornare arcu odio ut sem nulla pharetra diam sit. Ultrices sagittis orci a scelerisque. Ac felis donec et odio pellentesque diam volutpat commodo sed. Pulvinar pellentesque habitant morbi tristique senectus. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Ut consequat semper viverra nam libero justo. In vitae turpis massa sed elementum tempus egestas sed sed.
+          <div
+            style={{
+              width: "80%",
+              marginLeft: "auto",
+              marginRight: "auto",
+              fontSize: "1rem",
+              display: "inline",
+            }}
+          >
+            <h1>About Us</h1>
+                <div style={{ "fontSize": "20px", "marginLeft": "5px", "marginRight": "5px"}}>                                
+                    <div>
+                    I'd like to invite you to join me in a weekly odyssey, full of
+                    movies and good times and the occasional communal activity.
+                    We're not just going to survive, we're going to thrive thanks
+                    to the power of the LE CINEMA.
+                    </div>
+                    <br />
+                    <br />
+                    <div>
+                    Each week, we shall pick a movie we can all watch from a
+                    shortlist I'll send out every Saturday afternoon. Ideally
+                    it'll be a bit obscure or new so everybody will get to see it
+                    with fresh eyes. Then, based on everybody's availability we
+                    will use Discord to do a discussion of the film at the end of
+                    each week.
+                    </div>
+                    <br />
+                    <br />
+                    <em> - Russell Seyfert</em>
+                    <br />
+                    <br /> 
+                    <div style={{ textAlign: "center" }}>
+                    <img
+                        src={process.env.PUBLIC_URL + "/images/PFC-new-logo.png"}
+                        alt="PFC new logo"
+                        style={{ width: "400px", border: "1px solid black" }}
+                    />
+                    </div>
                 </div>
-                <div>
-                Sagittis vitae et leo duis ut diam. Venenatis cras sed felis eget velit aliquet. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Tincidunt ornare massa eget egestas purus viverra accumsan in. Sem viverra aliquet eget sit amet tellus. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Eleifend quam adipiscing vitae proin. Id diam maecenas ultricies mi. Maecenas sed enim ut sem viverra aliquet eget. Elit duis tristique sollicitudin nibh sit. Duis convallis convallis tellus id interdum velit laoreet.
-                </div>
-                <div>
-
-                </div>
-                <div>
-                Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Augue neque gravida in fermentum. Et netus et malesuada fames ac turpis egestas sed. Turpis nunc eget lorem dolor sed viverra ipsum. Vitae proin sagittis nisl rhoncus mattis rhoncus. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Viverra nam libero justo laoreet. Orci a scelerisque purus semper eget duis at tellus at. Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Aliquet lectus proin nibh nisl condimentum id venenatis. Nunc non blandit massa enim nec dui nunc.
-                </div>
-                <div>
-                Arcu bibendum at varius vel. Scelerisque eu ultrices vitae auctor eu augue ut lectus. Non arcu risus quis varius quam quisque id diam. Sit amet purus gravida quis blandit turpis cursus in hac. Sit amet venenatis urna cursus. Pellentesque dignissim enim sit amet venenatis urna cursus. Enim lobortis scelerisque fermentum dui faucibus in ornare. Nunc mi ipsum faucibus vitae aliquet. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Feugiat sed lectus vestibulum mattis ullamcorper velit sed. Diam in arcu cursus euismod quis viverra nibh cras pulvinar. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique.
-                </div>
-            </div>
+          </div>
         </MediaQuery>
         <MediaQuery minWidth={1000}>
-        <div>
+          <div>
             <div className="page-container">
-        <div className="page-left">
-            <aside>
-                <h1 style={{"fontSize": "100px", "marginTop": "200px"}}>About</h1>
-            </aside>
-            
-        </div>
-        
-        <div className="page-middle">
-            <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at elementum. Morbi enim nunc faucibus a pellentesque. Velit laoreet id donec ultrices tincidunt arcu non sodales. Ornare arcu odio ut sem nulla pharetra diam sit. Ultrices sagittis orci a scelerisque. Ac felis donec et odio pellentesque diam volutpat commodo sed. Pulvinar pellentesque habitant morbi tristique senectus. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Ut consequat semper viverra nam libero justo. In vitae turpis massa sed elementum tempus egestas sed sed.
+              <div className="page-left">
+                <aside>
+                  <h1 style={{ fontSize: "100px", marginTop: "200px" }}>
+                    About
+                  </h1>
+                </aside>
+              </div>
+
+              <div className="page-middle">
+                <div style={{ fontSize: "20px" }}>                                
+                    <div>
+                    I'd like to invite you to join me in a weekly odyssey, full of
+                    movies and good times and the occasional communal activity.
+                    We're not just going to survive, we're going to thrive thanks
+                    to the power of the LE CINEMA.
+                    </div>
+                    <br />
+                    <br />
+                    <div>
+                    Each week, we shall pick a movie we can all watch from a
+                    shortlist I'll send out every Saturday afternoon. Ideally
+                    it'll be a bit obscure or new so everybody will get to see it
+                    with fresh eyes. Then, based on everybody's availability we
+                    will use Discord to do a discussion of the film at the end of
+                    each week.
+                    </div>
+                    <br />
+                    <br />
+                    <em> - Russell Seyfert</em>
+                    <br />
+                    <br />
+                    <div style={{ textAlign: "center" }}>
+                    <img
+                        src={process.env.PUBLIC_URL + "/images/PFC-new-logo.png"}
+                        alt="PFC new logo"
+                        style={{ width: "600px", border: "1px solid black" }}
+                    />
+                    </div>
+                </div>
+              </div>
+              <div className="page-right">
+                <img
+                  id="lightbulb-gif"
+                  src={
+                    process.env.PUBLIC_URL +
+                    "./images/ezgif.com-video-to-gif (9).gif"
+                  }
+                  alt="abstract design"
+                />
+              </div>
             </div>
-            <div>
-            Sagittis vitae et leo duis ut diam. Venenatis cras sed felis eget velit aliquet. Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Tincidunt ornare massa eget egestas purus viverra accumsan in. Sem viverra aliquet eget sit amet tellus. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Eleifend quam adipiscing vitae proin. Id diam maecenas ultricies mi. Maecenas sed enim ut sem viverra aliquet eget. Elit duis tristique sollicitudin nibh sit. Duis convallis convallis tellus id interdum velit laoreet.
-            </div>
-            <div>
-                
-            </div>
-            <div>
-            Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Augue neque gravida in fermentum. Et netus et malesuada fames ac turpis egestas sed. Turpis nunc eget lorem dolor sed viverra ipsum. Vitae proin sagittis nisl rhoncus mattis rhoncus. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Viverra nam libero justo laoreet. Orci a scelerisque purus semper eget duis at tellus at. Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Aliquet lectus proin nibh nisl condimentum id venenatis. Nunc non blandit massa enim nec dui nunc.
-            </div>
-            <div>
-            Arcu bibendum at varius vel. Scelerisque eu ultrices vitae auctor eu augue ut lectus. Non arcu risus quis varius quam quisque id diam. Sit amet purus gravida quis blandit turpis cursus in hac. Sit amet venenatis urna cursus. Pellentesque dignissim enim sit amet venenatis urna cursus. Enim lobortis scelerisque fermentum dui faucibus in ornare. Nunc mi ipsum faucibus vitae aliquet. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Feugiat sed lectus vestibulum mattis ullamcorper velit sed. Diam in arcu cursus euismod quis viverra nibh cras pulvinar. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique.
-            </div>
-        </div>
-        <div className="page-right">
-            
-            <img id="lightbulb-gif" src={process.env.PUBLIC_URL + './images/ezgif.com-video-to-gif (9).gif'} alt="abstract design" />
-        </div>
-        </div>
-        <div style={{"bottom": "0", "left": "0", "marginBottom": "0px"}}>
-        </div>
-        </div>
+            <div style={{ bottom: "0", left: "0", marginBottom: "0px" }}></div>
+          </div>
         </MediaQuery>
-    </React.Fragment>
-    )
+      </React.Fragment>
+    );
 }
 
 export default About
